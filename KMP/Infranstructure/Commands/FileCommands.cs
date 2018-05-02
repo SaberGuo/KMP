c3f23d9c01be3999891bce1a505f9e19858bcb69
saber.guo <saber.guo@gmail.com>
2018-05-02 09:21:45 +0800

    add avalondock and xceed toolkit

---
+++ KMP/Infranstructure/Commands/FileCommands.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Practices.Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Infranstructure.Commands
+{
+    public static class FileCommands
+    {
+        //TODO: 03 - The application defines a global SaveAll command which invokes the Save command on all registered Orders. It is enabled only when all orders can be saved.
+        public static CompositeCommand OpenFileCommand = new CompositeCommand();
+        public static CompositeCommand NewFileCommand = new CompositeCommand();
+        public static CompositeCommand SaveAllFileCommand = new CompositeCommand();
+    }
+
+    /// <summary>
+    /// Provides a class wrapper around the static SaveAll command.
+    /// </summary>
+    public class FileCommandProxy
+    {
+        public virtual CompositeCommand OpenFileCommand
+        {
+            get { return FileCommands.OpenFileCommand; }
+        }
+
+        public virtual CompositeCommand SaveAllFileCommand
+        {
+            get { return FileCommands.SaveAllFileCommand; }
+        }
+    }
+}
---
+++ KMP/KMP.Parameterization/InventorMonitor/InvMonitorViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace KMP.Parameterization.InventorMonitor
+{
+    class InvMonitorViewModel: INotifyPropertyChanged
+    {
+        public InvMonitorViewModel(string filePath)
+        {
+
+        }
+        
+        public string FileName { get; set; }
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+    }
+}
