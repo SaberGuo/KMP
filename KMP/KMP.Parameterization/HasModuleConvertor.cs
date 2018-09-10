7da2c8a62e950259f6c99fcfbcc6c08a62640174
saber.guo <saber.guo@gmail.com>
2018-09-10 22:10:43 +0800

    add report part

---
+++ KMP/KMP.Parameterization/HasModuleConvertor.cs
@@ -0,0 +1,32 @@
+﻿using KMP.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KMP.Parameterization
+{
+    public class HasModuleConvertor : System.Windows.Data.IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            IParamedModule param = value as IParamedModule;
+            if (param == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+            
+
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
